293fa879912714108e07d46df2330b471f54ba4b
unknown <ben.trevethan@gmail.com>
2020-12-11 21:34:58 +1030

    added test page temp_test.html. added variables for numeric, upper and lower case and special characters.

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+// vars declared with console.log to check functionality
+var alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
+  console.log(alphabet);
+var alphabetUpper = "abcdefghijklmnopqrstuvwxyz".toUpperCase("").split("");
+  console.log(alphabetUpper);
+// special chars exluding " " (space)
+var specialChar = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~".split(""); 
+  console.log(specialChar);
+var numb = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+  console.log(numb);
+
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
 
 }
