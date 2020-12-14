75dac54cb880fda5e51108df6873f89ae1e4620e
unknown <ben.trevethan@gmail.com>
2020-12-14 12:45:18 +1030

    created for loops for pushing into charArray based on inputs (boolean) in generatePassword

--- script.js
+++ script.js
@@ -7,10 +7,8 @@ var alphabetUpper = "abcdefghijklmnopqrstuvwxyz".toUpperCase("").split("");
 console.log(alphabetUpper);
 var specialChar = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~".split("");
 console.log(specialChar);
-var numb = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var numb = [0,1,2,3,4,5,6,7,8,9];
 console.log(numb);
-var pwLength;
-
 
 // Write password to the #password input
 function writePassword() {
@@ -22,44 +20,56 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 function writePassword() {
-  // wrapping length prompt in function to call back to when false
-  var pwLength = prompt("How many characters, between 8 and 128, will your password have?");
+  // wrapping charLength prompt in function to call back to when false
+  var pwLength = prompt(
+    "How many characters, between 8 and 128, will your password have?"
+  );
   var pwLength = parseInt(pwLength);
-    if (pwLength >= 8 && pwLength <= 128) {
+  console.log("length is " + pwLength);
+  if (pwLength >= 8 && pwLength <= 128) {
     console.log(pwLength);
-    } 
-    else {
+  } else {
     alert("Must be between 8 and 128 characters");
-    console.log("false");
-    writePassword()
-    }
+    console.log(pwLength);
+    writePassword();
+  }
   var capital = confirm("Allow capital letters?");
-    if (capital === true) {
-      console.log(capital);
-    }
-    else {
-      console.log(capital);
-    }
+  console.log(capital);
   var specialCharBool = confirm("Allow special characters? @, %, ^");
-    if (specialCharBool === true) {
-      console.log(specialCharBool);
-    }
-    else {
-      console.log(specialCharBool);
-    }
-  var numbers = confirm("Allow numbers 0-9?")
-    if (numbers === true) {
-      console.log(numbers);
-    }
-    else {
-      console.log(numbers);
-    }
-    generatePassword()
-    function generatePassword() {
-      console.log(pwLength);
-      console.log(capital);
-      console.log(specialCharBool);
-      console.log(numbers);
+  console.log(specialCharBool);
+  var numbers = confirm("Allow numbers 0-9?");
+  console.log(numbers);
 
+  generatePassword();
+  function generatePassword() {
+    // log to check functionality of vars
+    //console.log(pwLength, capital, specialCharBool, numb);
+    var charArray = [];
+      if (numbers) for (i = 0; i < numb.length; i++) {
+      charArray.push(numb[i]);
+      }
+      console.log(charArray);
+      if (specialCharBool) for (j = 0; j < specialChar.length; j++) {
+      charArray.push(specialChar[j])
+      }
+      console.log(charArray);
+      if (capital) for (k = 0; k < alphabetUpper.length; k++) {
+      charArray.push(alphabetUpper[k]);
+      }
+      console.log(charArray);
+      //always push alphabet
+      for (m = 0; m < alphabet.length; m++) {
+        charArray.push(alphabet[m]);
+      }
+      console.log(charArray);
+
+
+
+    var password = "";
+      for (n = 0; n < parseInt(pwLength); n++) {
+        password += (charArray[Math.floor(Math.random() * charArray.length)]);
     }
-}
+    console.log(password);
+  }
+  passwordText.value = password;
+  }
